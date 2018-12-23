b66016e279f234e25bc35d9d6a646cf0f5ed9555
wisdomtooth <cbtsao47@gmail.com>
2018-12-22 18:06:34 -0600

    add index.html

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Function constructor
 
-var john = {
-  name: "john",
-  yearOfBirth: 1990,
-  job: "teacher"
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+  this.calculateAge = function() {
+    console.log(this);
+  };
 };
+
+//instanciation, john is an instance of the Person constructor function
+var john = new Person("john", "1990", "teacher");
+
+john.calculateAge();
+
+var jane = new Person("jane", "1969", "designer");
+
+var mark = new Person("mark", "1948", "retired");
