2035a3c9f1d74415fa6b9878f4d543a4aa4517f2
wisdomtooth <cbtsao47@gmail.com>
2018-12-22 20:44:56 -0600

    add callback practice

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ var mark = new Person("mark", "1948", "retired");
 jane.calculateAge();
 mark.calculateAge();
 
-console.log(Person.prototype);
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(jane);
+// console.log(Person.prototype);
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(jane);
 
 // Object.create
 
@@ -50,3 +50,29 @@ var jenny = Object.create(peersonProto, {
 
 // function constructor makes the newly created object inherit the properties from the prototype
 // Object.create specifies the prototype before the object is created
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i]));
+  }
+  return result;
+}
+
+function calculateAge(birthYear) {
+  return 2018 - birthYear;
+}
+
+function isFullAge(age) {
+  return age >= 18;
+}
+function maxHeartRate(currentAge) {
+  if (currentAge >= 18 && currentAge <= 81) {
+    return Math.round(206.9 - 0.67 * currentAge);
+  } else return -1;
+}
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var heartRate = arrayCalc(ages, maxHeartRate);
