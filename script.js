ff36b2fbda0378be0a188e947f0f66e6149a5fb5
wisdomtooth <cbtsao47@gmail.com>
2018-12-22 18:52:27 -0600

    learning constructor vs prototype

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = function() {
-    console.log(this);
+  this.test = function() {
+    console.log("test");
   };
 };
-
+Person.prototype.calculateAge = function() {
+  console.log(2018 - this.yearOfBirth);
+};
+Person.prototype.lastName = "name";
 //instanciation, john is an instance of the Person constructor function
 var john = new Person("john", "1990", "teacher");
 
@@ -17,3 +20,11 @@ john.calculateAge();
 var jane = new Person("jane", "1969", "designer");
 
 var mark = new Person("mark", "1948", "retired");
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(Person.prototype);
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(jane);
+console.log(Person.constructor);
