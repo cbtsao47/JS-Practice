828c56b6b5bbd768eddc6df5e7867a49bab87071
wisdomtooth <cbtsao47@gmail.com>
2018-12-22 19:37:42 -0600

    add constructor vs object.create difference

--- script.js
+++ script.js
@@ -27,4 +27,26 @@ console.log(Person.prototype);
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(jane);
-console.log(Person.constructor);
+
+// Object.create
+
+var peersonProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth);
+  }
+};
+
+var johnny = Object.create(peersonProto);
+
+johnny.name = "johnny";
+johnny.yearOfBirth = 1990;
+johnny.job = "tester";
+var jenny = Object.create(peersonProto, {
+  name: { value: "jenny" },
+  yearOfBirth: { value: 1969 },
+  job: { value: "designer" },
+  testing: { value: 123 }
+});
+
+// function constructor makes the newly created object inherit the properties from the prototype
+// Object.create specifies the prototype before the object is created
